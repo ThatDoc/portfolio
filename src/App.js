a615aa19d9f7927cb4393409edcca1510ca65193
Doc <drk.productioons@gmail.com>
2025-03-06 13:04:23 +0100

    created react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.jpg';
+import './index.css'
+import './App.css';
+
+function App() {
+  return (
+    <div className="main">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <a>
+          hardcore
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
