79441b91f47aeb6557b659bd5701d7116178a2eb
Doc <82221725+ThatDoc@users.noreply.github.com>
2025-04-07 23:04:39 +0200

    Version 1.3.9
    experiences info added

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -17,6 +17,52 @@ const StartPage = () => {
                     , hence the two sections on here. I'm also really into cinema and a huge fan of 
                     public transit, apparently.
                 </p>
+                <h3>
+                    <b>Work Experience</b>
+                </h3>
+                <p>
+                    <ul>
+                        <li>
+                            2021 - 2023
+                            Solo indie game development
+                        </li>
+                        <li>
+                            2023 - now <a className="link" href="https://deezgames.eu">Deez Games</a>
+                            <br/>
+                            Programmer, Artist, Technical Artist
+                        </li>
+                    </ul>
+                </p>
+                <h3>
+                    <b>Education</b>
+                </h3>
+                <p>
+                    <ul>
+                        <li>
+                            2019-2024 Technical School no. 4 in Gdansk
+                            <br/>
+                            Technician - Programmer
+                        </li>
+                        <li>
+                            2024 - now Gdansk University of Technology
+                            <br/>
+                            Informatics
+                        </li>
+                    </ul>
+                </p>
+                <h3>
+                    <b>Languages</b>
+                </h3>
+                <p>
+                    <ul>
+                        <li>
+                            Polish - native
+                        </li>
+                        <li>
+                            English - CAE Grade A, C2
+                        </li>
+                    </ul>
+                </p>
             </div>
         </div>
     );
