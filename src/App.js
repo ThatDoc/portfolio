712f6e3b1cb20daf6e967319c38c7d2b7e78d2b1
Doc <drk.productioons@gmail.com>
2025-05-02 01:48:14 +0200

    film roll on start page & est info

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ const App = () => {
               <Route path="/Gamedev" element={<GamedevPage />} />
             </Routes>
         </div>
+        <p className='App-est'>EST. MMXXV</p>
       </div>
     </BrowserRouter>
   );
