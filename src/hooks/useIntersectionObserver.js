dcafcf56d0b76a3e241b32f986c876893a3d7dbe
Doc <drk.productioons@gmail.com>
2025-05-02 15:04:28 +0200

    text fade in fix

--- src/hooks/useIntersectionObserver.js
+++ src/hooks/useIntersectionObserver.js
@@ -5,9 +5,11 @@ const useIntersectionObserver = (selector, animation, shouldRun = true, options 
         if (!shouldRun) return;
 
         const items = document.querySelectorAll(selector);
+        items.forEach(item => item.classList.add("hidden"));
         const observer = new IntersectionObserver((entries, obs) => {
           entries.forEach(entry => {
             if (entry.isIntersecting && !entry.target.classList.contains('animate')) {
+              entry.target.classList.remove("hidden");
               entry.target.classList.add(animation);
               obs.unobserve(entry.target);
             }
